2516f277c40701aeea73354d6cee68f32c6ad1c1
Edwedja de Lima <90485665+Edwedja@users.noreply.github.com>
2022-01-22 22:21:28 -0300

    Update sketch.js
    Oi Nat!! ^^ 
    Então o maior problema era na aplicação da força rsrs (linha 56), 
    o x é minúsculo e positivo, para que a bola vá pra frente. 
    Além disso, como o objeto tem densidade, a força aplicada tem que ser maior, se não ele não se mexe! bjsss

--- sketch.js
+++ sketch.js
@@ -5,7 +5,6 @@ const Body = Matter.Body;
 
 var ball;
 var groundObj
-
 var leftSide,rightSide;
 var world, engine;
 
@@ -24,17 +23,14 @@ function setup() {
 		density:1.2
 	}
 	
-	
-	
-	
 	ball = Bodies.circle(50,300,25,ball_options);
 	World.add(world,ball);
 
 	
 	//Crie o chão e as paredes esquerda e direita (USANDO A CLASSE ground.js)
-groundObj = new ground(800, 600, 1600, 20);
-rightSide = new ground(1100,600,20,200);
-leftSide = new ground(850,600,20,200);
+	groundObj = new ground(800, 670, 1600, 20); // Baixei um pouco mais o chão
+	rightSide = new ground(1100,600,20,200);
+	leftSide = new ground(850,600,20,200);
 
 	Engine.run(engine);
 }
@@ -45,18 +41,19 @@ function draw() {
   
 
  // desenha a bola aqui (ellipse...)
-    ellipse(ball.position.x,ball.position.y,25)
+    ellipse(ball.position.x,ball.position.y,50,50) //como o raio é 25, o ellipse vai precisar do diametro duas vezes! na aula te explico o porquê!
  // desenhe o chão e paredes aqui (display)
  //groundObj.display();
-groundObj.display();
-rightSide.display();
-leftSide.display();
+ groundObj.display();
+ rightSide.display();
+ leftSide.display();
 }
 
 // SE precionar a tela UP -> deve ser aplicada uma força na bola
 function keyPressed() {
   	if (keyCode === UP_ARROW) {
 		// Aplique a força aqui
-		Matter.Body.applyForce(ball, ball.position, {X:-0.001,y:-0.001})
+		Matter.Body.applyForce(ball, ball.position, {x:80,y:-80}) // o problema estava nessa linha, o x é minusculo e positivo rsrs,
+		
   	}
 }
